e503450c0ed8fbd16247a578343961b33c16797d
Marbelly <me0763032023@unab.edu.sv>
2024-08-27 10:45:45 -0600

    Creacion de Modelos.

--- Principal.Designer.cs
+++ Principal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CRUDPARCIAL1
 {
-    partial class Form1
+    partial class Principal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Principal";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace CRUDPARCIAL1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Principal());
         }
     }
 }
